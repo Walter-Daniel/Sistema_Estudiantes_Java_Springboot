a4bb89ac16061a030364e3471e3d82e226d9fadc
Walter Daniel Carrizo <99771018+Walter-Daniel@users.noreply.github.com>
2025-06-30 23:20:03 -0300

    Initial commit: config dependencies and database

---
+++ src/test/java/purple/school/SchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package purple.school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
