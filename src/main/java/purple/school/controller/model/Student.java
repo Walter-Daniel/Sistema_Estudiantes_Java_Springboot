022dfceadb011a03a906f8d95295678ce48990dc
Walter Daniel Carrizo <99771018+Walter-Daniel@users.noreply.github.com>
2025-07-01 21:52:26 -0300

    feat: add Student entity with basic attributes

---
+++ src/main/java/purple/school/controller/model/Student.java
@@ -0,0 +1,28 @@
+package purple.school.controller.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+// boilerplate - repetitivo
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idStudent;
+
+    private String firstName;
+    private String lastName;
+    private String numberPhone;
+    private String email;
+}
